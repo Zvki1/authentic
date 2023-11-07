c13aac76652d9ccdfa443eaaafcd6c2b80dbb883
Zvki1 <rzaki@hotmail.fr>
2023-11-07 22:00:14 +0100

    adding the design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'manrope': ['Manrope', 'sans'],
+        'poppins': ['Poppins', 'sans'],
+      },
+      colors:{
+        "Neutral":{
+          "Black":"#000000",
+          "90":"252E36",
+          "50":"A4B0C1",
+          "20":"E3E4E4",
+          "10":"F9F9F9",
+          "White":"#FFFFFF",
+        },
+        "Primary":{
+        "Night-Blue":"#012A65",
+        "Ocean-Blue":"#477FF6",
+        "Sky-Blue":"#68A6FD",
+        "Cloud-Blue":"#CDE1FE",
+        },
+        "Secondary":{
+          "Tchina-Orange":"#FF731D",
+          "Mandarin-Orange":"#F9A526",
+          "Success":"#5CFF8A",
+          "ERROR-404":"#FF5C5C",
+          },
+      },
+      
+    },
   },
   plugins: [],
 };
